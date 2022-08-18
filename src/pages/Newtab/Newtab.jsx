732e72df127cd60e25629937a1feaf66722c051a
Vladyslav <verbalike@gmail.com>
2022-08-18 20:33:32 +0300

    locatlisation

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import './index.scss';
 
 import MainView from './components/main_view';
+import { useEffect } from 'react';
 const Newtab = () => {
+
+  useEffect(() => {
+    console.log(chrome.i18n.getMessage('general'));
+    // chrome.storage.sync.set({key: value}, function() {
+    //   console.log('Value is set to ' + value);
+    // });
+
+    // chrome.storage.sync.get(['key'], function(result) {
+    //   console.log('Value currently is ' + result.key);
+    // });
+  }, [])
+
   return (
     <div className="App">
       <MainView />
