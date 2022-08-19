9b55b1a43f0f3f30c7ab7e6644de004cc8a8e12b
Vladyslav <verbalike@gmail.com>
2022-08-19 15:15:35 +0300

    pull request fixed

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -4,6 +4,18 @@ import './index.scss';
 import MainView from './components/main_view';
 import { useEffect } from 'react';
 const Newtab = () => {
+
+  useEffect(() => {
+    console.log(chrome.i18n.getMessage('general'));
+    // chrome.storage.sync.set({key: value}, function() {
+    //   console.log('Value is set to ' + value);
+    // });
+
+    // chrome.storage.sync.get(['key'], function(result) {
+    //   console.log('Value currently is ' + result.key);
+    // });
+  }, [])
+
   return (
     <div className="App">
       <MainView />
