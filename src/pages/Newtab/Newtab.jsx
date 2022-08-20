2ddab98bb85bf4eb27a025ee12e5874ce9c63d5a
F1rman <ihorkharchyshyn@gmail.com>
2022-08-20 13:07:20 +0300

    draggable items

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -3,39 +3,48 @@ import './index.scss';
 
 import MainView from './components/main_view';
 import { useEffect } from 'react';
-import I18n, { getLocalisations } from '../../components/Translation/Translation';
+import I18n, {
+  getLocalisations,
+} from '../../components/Translation/Translation';
 export const UserContext = createContext();
 
 export const initial_store = {
   l10n: {},
   settings: {
-    lang: 'uk', theme: 'dark', search_box: {
-      color: '#202124', font_color: '#202124', icon_color: '#202124'
+    lang: 'uk',
+    theme: 'dark',
+    search_box: {
+      color: '#202124',
+      font_color: '#202124',
+      icon_color: '#202124',
     },
     time: {
-      delay: 20000, format: 24
+      delay: 20000,
+      format: 24,
     },
     background: { custom: null, selected: 0 },
     folders: [
       {
-        name: 'Welcome', font_color: '#202124', preview: null, bookmarks: [
-          { position: { x: 4, y: 4 }, name: null, url: null, preview: null },
+        name: 'Welcome',
+        font_color: '#202124',
+        preview: null,
+        bookmarks: [
+          { position: { x: 4, y: 4 }, name: 'null', url: null, preview: null },
           { position: { x: 5, y: 4 }, name: null, url: null, preview: null },
           { position: { x: 6, y: 4 }, name: null, url: null, preview: null },
           { position: { x: 7, y: 4 }, name: null, url: null, preview: null },
-          { position: { x: 8, y: 4 }, name: null, url: null, preview: null }
-        ]
-      }
-    ]
-  }
+          { position: { x: 8, y: 4 }, name: null, url: null, preview: null },
+        ],
+      },
+    ],
+  },
 };
 const updated_localisation_store = getLocalisations({ store: initial_store });
 const Newtab = () => {
   const [store, setStore] = useState(updated_localisation_store);
 
-
   useEffect(() => {
-    console.log(store)
+    console.log(store);
     getLocalisations({ store, setStore });
     // chrome.storage.sync.set({key: value}, function() {
     //   console.log('Value is set to ' + value);
@@ -44,16 +53,15 @@ const Newtab = () => {
     // chrome.storage.sync.get(['key'], function(result) {
     //   console.log('Value currently is ' + result.key);
     // });
-  }, [])
+  }, []);
 
   return (
-    < UserContext.Provider value={{ store, setStore }
-    }>
+    <UserContext.Provider value={{ store, setStore }}>
       {/* <I18n>general</I18n> */}
       <div className="App">
         <MainView />
       </div>
-    </UserContext.Provider >
+    </UserContext.Provider>
   );
 };
 
---
+++ src/pages/Newtab/components/grid_layout.jsx
@@ -0,0 +1,62 @@
+import React, { useContext, useState } from 'react';
+
+import '/node_modules/react-grid-layout/css/styles.css';
+import '/node_modules/react-resizable/css/styles.css';
+import RGL, { WidthProvider } from 'react-grid-layout';
+import { UserContext } from '../Newtab';
+import SearchForm from './search_form';
+import ReactGridLayoutItem from './react_grid_layout_item';
+const ReactGridLayout = WidthProvider(RGL);
+const GridLayout = (props) => {
+  const { store } = useContext(UserContext);
+  console.log(store.settings);
+  var layout = [{ i: 'searchBar', x: 4, y: 3, w: 5, h: 1, static: true }];
+  store.settings.folders.forEach((folder) => {
+    folder.bookmarks.forEach((bookmark, bookmarkId) =>
+      layout.push({
+        i: bookmarkId + '-' + bookmark.name,
+        x: bookmark.position.x,
+        y: bookmark.position.y,
+        w: 1,
+        h: 1,
+        title: bookmark.name,
+      })
+    );
+  });
+  for (let x = 0; x < 13; x++) {
+    layout.push({ i: 'astatic' + x, x: x, y: 8, w: 1, h: 2, static: true });
+  }
+  function renderSwitch(e) {
+    if (e.i.includes('astatic')) {
+      return <div></div>;
+    }
+    switch (e.i) {
+      case 'searchBar':
+        return <SearchForm />;
+
+      default:
+        return (
+          <ReactGridLayoutItem openModal={props.openModal} title={e.title} />
+        );
+    }
+  }
+  return (
+    <ReactGridLayout
+      className="layout"
+      layout={layout}
+      cols={13}
+      rowHeight={(window.innerHeight - 40) / 8}
+      isBounded={true}
+      verticalCompact={false}
+      preventCollision={true}
+      isResizable={false}
+      margin={[0, 0]}
+    >
+      {layout.map((e) => (
+        <div key={e.i}>{renderSwitch(e)}</div>
+      ))}
+    </ReactGridLayout>
+  );
+};
+
+export default GridLayout;
