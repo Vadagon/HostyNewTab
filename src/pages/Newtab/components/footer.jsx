5f9bb2ab2aab26cc650a0acfb2a823f3b0210a37
Vadagon <verbalike@gmail.com>
2022-12-02 14:18:23 +0200

    name changed + privacy link added

--- src/pages/Newtab/components/footer.jsx
+++ src/pages/Newtab/components/footer.jsx
@@ -15,6 +15,9 @@ const Footer = (props) => {
       <a href={'https://mainbox.info/contact/'} rel={'noreferrer'} className="text-[#ffffff8a] mr-3">
         Contact Us
       </a>
+      <a href={'https://mainbox.info/'} rel={'noreferrer'} className="text-[#ffffff8a] mr-3">
+        About Us
+      </a>
     </div>
   );
 };
